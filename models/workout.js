e31f070a0db6d4a0bc9cd6d50f9c70732bebb6c1
Steven Stefanov <77702697+stevenstefanov@users.noreply.github.com>
2021-05-26 16:58:09 -0400

    pushing workout.js.

---
+++ models/workout.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now()
+    },
+    exercises: [
+        {
+            type: { type: String },
+            name: String,
+            distance: Number,
+            durantion: Number,
+            weight: Number,
+            sets: Number,
+            reps: Number,
+        }
+    ],
+    totalDuration: {
+        type: Number,
+        default: 0,
+    }
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
